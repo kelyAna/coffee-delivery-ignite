cc15e2901fb26e5e84747cb4a85d2248b86c03f1
kelyAna <kely.lopes@vtex.com.br>
2022-09-16 21:41:03 -0300

    feat: add IntroSection component

--- src/components/IntroSection/styles.ts
+++ src/components/IntroSection/styles.ts
@@ -20,9 +20,28 @@ export const TitleSection = styled.div`
     font-size: 1.25rem;
     color: #403937;
     font-stretch: 100;
+    color: #403937;
+    font-style: normal;
   }
 `
 
 export const ItemsSection = styled.div`
- 
+  margin-top: 66px;
+  display: flex;
+  gap: 40px;
+`
+export const ItemSection = styled.div`
+  align-items: center;
+`
+
+export const Item = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 25px;
+
+  gap: 12px;
+
+  span {
+    font-size: 1rem;
+  }
 `
