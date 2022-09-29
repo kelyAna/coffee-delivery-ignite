992287c5f24022d83034ead4b766275f38857cae
kelyAna <kely.lopes@vtex.com.br>
2022-09-28 22:53:57 -0300

    feat: add ItemCard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from 'react-router-dom'
 import { Header } from './components/Header'
 import { IntroSection } from './components/IntroSection'
+import { ItemsList } from './components/ItemsList'
 import './global.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Header />
       <IntroSection />
+      <ItemsList />
     </BrowserRouter>
   )
 }
---
+++ src/components/ItemsList/components/ItemCard/styles.ts
@@ -0,0 +1,109 @@
+import styled from 'styled-components'
+
+export const ItemCardContainer = styled.div`
+  width: 256px;
+  height: 310px;
+
+  text-align: center;
+  background: #F3F2F2;
+  border-radius: 6px 36px;
+
+  align-items: center;
+  justify-content: center;
+
+  padding: 0px 24px 20px 24px;
+`
+
+export const ItemCardContainerHeader = styled.h2`
+  font-weight: 800;
+  margin-bottom: 2.125rem;
+
+  font-family: 'Baloo 2';
+`
+
+export const ItemCardTagList = styled.div`
+  display: flex;
+
+  align-items: center;
+  justify-content: center;
+
+  margin-top: 0.75rem;
+`
+
+export const ItemCartImage = styled.img`
+  margin-top: -20px;
+
+  width: 7.5rem;
+  height: 7.5rem;
+`
+
+export const ItemCardTag = styled.div`
+  background: var(--yellow-100);
+  padding: 0.25rem 0.5rem;
+
+  font-size: 0.625rem;
+  font-weight: 700;
+
+  border-radius: 100px;
+  color: var(--yellow-dark);
+`
+
+export const ItemNameCard = styled.p`
+  font-family: 'Baloo 2';
+  font-size: 1.25rem;
+  font-weight: 700;
+`
+
+export const ItemDescriptionCard = styled.p`
+  font-size: 0.875rem;
+  font-style: normal;
+
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 33px;
+
+  max-width: 200px;
+
+  color: var(--base-title);
+`
+
+export const ItemBuyCard = styled.div`
+  display: flex;
+
+  justify-content: space-between;
+  padding: 0px 24px 0px 24px;
+
+  height: 38px;
+`
+
+export const ItemPriceCard = styled.div`
+  font-family: 'Roboto';
+  font-weight: 400;
+
+  font-size: 0.875rem;
+
+  align-items: center;
+
+  display: flex;
+  gap: 0.2rem;
+
+  color: var(--base-text);
+`
+
+export const ItemActionsCard = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  flex-direction: row;
+
+  gap: 0.5rem;
+`
+
+export const ItemCart = styled.div`
+  padding: 0.4rem;
+  border-radius: 6px;
+  
+  background-color: var(--purple-400);
+
+  color: white;
+`
