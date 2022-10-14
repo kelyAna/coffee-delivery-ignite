2cd4dbf5cb4cd5888b4522799441bbdc15567c28
kelyAna <kely.lopes@vtex.com.br>
2022-10-13 22:41:57 -0300

    chore: code improvements

--- src/screens/Checkout/components/OrderDataForm/index.tsx
+++ src/screens/Checkout/components/OrderDataForm/index.tsx
@@ -1,11 +1,19 @@
-import { AddressForm, AddressFormContainer, InputAddressForm, LineForm, OrderDataFormContainer, PaymentForm, TitleSection } from "./styles"
+import {
+  AddressForm,
+  AddressFormContainer,
+  InputAddressForm,
+  LineForm,
+  OrderDataFormContainer,
+  PaymentForm,
+  TitleSection,
+} from './styles';
 
-import iconLocalization from './assets/Icon-5.svg'
-import creditCardIcon from './assets/Icon-7.svg'
-import debitCardIcon from './assets/Icon-8.svg'
-import moneyIcon from './assets/Icon-9.svg'
+import iconLocalization from './assets/Icon-5.svg';
+import creditCardIcon from './assets/Icon-7.svg';
+import debitCardIcon from './assets/Icon-8.svg';
+import moneyIcon from './assets/Icon-9.svg';
 
-import paymentIcon from './assets/Icon-6.svg'
+import paymentIcon from './assets/Icon-6.svg';
 
 export const OrderDataForm = () => {
   return (
@@ -13,8 +21,8 @@ export const OrderDataForm = () => {
       <TitleSection>Complete seu pedido</TitleSection>
 
       <AddressFormContainer>
-       <AddressForm>
-        <div>
+        <AddressForm>
+          <div>
             <LineForm>
               <img src={iconLocalization} alt="" />
               <div>
@@ -25,41 +33,71 @@ export const OrderDataForm = () => {
           </div>
           <InputAddressForm placeholder="CEP" />
           <InputAddressForm width="580px" placeholder="Rua" />
+
           <LineForm>
             <InputAddressForm placeholder="Número" />
-            <InputAddressForm width="348px" placeholder="Complemento Opcional" />
+            <InputAddressForm
+              width="348px"
+              placeholder="Complemento Opcional"
+            />
           </LineForm>
+
           <LineForm>
             <InputAddressForm placeholder="Bairro" />
             <InputAddressForm placeholder="Cidade" />
             <InputAddressForm width="110px" placeholder="UF" />
           </LineForm>
-       </AddressForm>
+        </AddressForm>
       </AddressFormContainer>
 
       <AddressFormContainer>
-       <AddressForm>
-        <div>
+        <AddressForm>
+          <div>
             <LineForm>
               <img src={paymentIcon} alt="" />
               <div>
                 <p>Pagamento</p>
-                <span>O pagamento é feito na entrega. Escolha a forma que deseja pagar</span>
+                <span>
+                  O pagamento é feito na entrega. Escolha a forma que deseja
+                  pagar
+                </span>
               </div>
             </LineForm>
           </div>
           <PaymentForm>
-            <input type="radio" id="radioCreditCard" name="radio" value="radioCreditCard" />
-            <label htmlFor="radioCreditCard"><img src={creditCardIcon} alt="" /> CARTÃO DE CRÉDITO</label>
+            <input
+              type="radio"
+              id="radioCreditCard"
+              name="radio"
+              value="radioCreditCard"
+            />
+            <label htmlFor="radioCreditCard">
+              <img src={creditCardIcon} alt="" /> CARTÃO DE CRÉDITO
+            </label>
 
-            <input type="radio" id="radioDebitCard" name="radio" value="radioDebitCard" />
-            <label htmlFor="radioDebitCard"><img src={debitCardIcon} alt="" /> CARTÃO DE DÉBITO</label>
+            <input
+              type="radio"
+              id="radioDebitCard"
+              name="radio"
+              value="radioDebitCard"
+            />
+            <label htmlFor="radioDebitCard">
+              <img src={debitCardIcon} alt="" /> CARTÃO DE DÉBITO
+            </label>
 
-            <input type="radio" id="radioMoney" name="radio" value="radioMoney" />
-            <label htmlFor="radioMoney"><img src={moneyIcon} alt="" />DINHEIRO</label>
+            <input
+              type="radio"
+              id="radioMoney"
+              name="radio"
+              value="radioMoney"
+            />
+            <label htmlFor="radioMoney">
+              <img src={moneyIcon} alt="" />
+              DINHEIRO
+            </label>
           </PaymentForm>
-       </AddressForm>
+        </AddressForm>
       </AddressFormContainer>
     </OrderDataFormContainer>
-  )
-}
+  );
+};
